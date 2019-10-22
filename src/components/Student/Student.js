5c231a96aeddf1f5a9a3d85d37374f6079f1e68e
L J <laprje@gmail.com>
2019-10-22 16:42:30 -0600

    finished project

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 export default class Student extends Component {
   constructor() {
     super()
+    this.state ={
+      studentInfo: {}
+    }
 
   }
 
-  render() {
+  componentDidMount(props) {
+    axios
+    .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+    .then(res => {
+      this.setState({
+        studentInfo: res.data
+      })
+    })
+  }
+
+  // goBack = () => {
+  //   window.history.back();
+  // }
+
+  render(props) {
     return (
       <div className="box">
-        <h1>Student</h1>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"></link>
+        <button className="backButton" onClick={this.props.history.goBack} icon='refresh' look='flat'>Back</button>
+        <h1>{this.state.studentInfo.first_name}{this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade} </h3>
+        <h3>Email: {this.state.studentInfo.email} </h3>
       </div>
     )
   }
